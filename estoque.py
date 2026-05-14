fed0e4eedf08c082d2debf4d2c1abbcda15a027f
Lucimar02S <lucimarmay@gmail.com>
2026-05-14 20:27:38 -0300

    adicionando estoque

---
+++ estoque.py
@@ -0,0 +1,6 @@
+def contar_sacas(entrada, saida):
+    return entrada - saida
+
+resultado = contar_sacas(100, 25)
+
+print("Quantidade restante no estoque:", resultado)
---
+++ teste_estoque.py
@@ -0,0 +1,5 @@
+from estoque import contar_sacas
+
+assert contar_sacas(100, 25) == 75
+
+print("Teste do estoque executado com sucesso")
